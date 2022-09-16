f0e764c74cb698d684e486b55a49a70d2ddb25b2
abadeer afif <77312611+abadeerafif@users.noreply.github.com>
2022-09-16 15:54:54 +0200

    login logic

--- gawala-website/src/components/login.js
+++ gawala-website/src/components/login.js
@@ -2,8 +2,33 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBj2LRr_SmVP1Aw7lxid8xVo6wy77jFha0",
+    authDomain: "gawaladahabcamp.firebaseapp.com",
+    projectId: "gawaladahabcamp",
+    storageBucket: "gawaladahabcamp.appspot.com",
+    messagingSenderId: "982432386374",
+    appId: "1:982432386374:web:4bf358a4cc63c6efdae947"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+
+  
 
 const Lggincom = () => {
+    const [email, setemail] = React.useState("");
+    const [pass, setpass] = React.useState("");
+    const handleChangemail = event => {
+        setemail(event.target.value);
+    }
+    const handleChangepass = event => {
+        setpass(event.target.value);
+    }
   
   return (
     <Box
@@ -20,6 +45,8 @@ const Lggincom = () => {
           required
           id="standard-required"
           label="Email"
+          value={email}
+          onChange={handleChangemail}
           
           variant="standard"
         />
@@ -27,10 +54,26 @@ const Lggincom = () => {
           required
           id="standard-required"
           label="Password"
-          
+          value={pass}
+          onChange={handleChangepass}
           variant="standard"
         />
-        <Button variant="contained">Login</Button>
+        <Button onClick={() => {
+            signInWithEmailAndPassword(auth, email, pass)
+            .then((userCredential) => {
+              // Signed in 
+              const user = userCredential.user;
+              console.log(user);
+              // ...
+            })
+            .catch((error) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              console.log(errorMessage);
+              
+            });
+    
+  }} variant="contained">Login</Button>
        
       </div>
     </Box>
