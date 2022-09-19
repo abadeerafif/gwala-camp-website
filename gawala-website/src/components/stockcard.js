bcb5be8feb6c59c7a90695f81e17c42a994d8d4c
abadeer afif <77312611+abadeerafif@users.noreply.github.com>
2022-09-19 05:33:20 +0200

    Revert "revertto stable"
    This reverts commit 8250552e00e1946f9ac8cb839bcfa3913e55d7f9.

--- gawala-website/src/components/stockcard.js
+++ gawala-website/src/components/stockcard.js
@@ -7,41 +7,85 @@ import { Button, CardActionArea, CardActions } from '@mui/material';
 import {buystock} from '../firebase interface/getstocks'
 import {userid,userstocks,usermoney} from '../firebase interface/sessionstate'
 
-export default function MultiActionAreaCard({name,price,image,desc,numbersold,total,refrefn}) {
-  return (
-    <Card sx={{ maxWidth: 400 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="300"
-          image={image}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-           {desc}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            {"Sold : "+numbersold+' / '+total}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            {price+'$'}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" onClick={async () => {
-            await buystock(userid,usermoney,price,name)
-            refrefn()
+export default function MultiActionAreaCard({name,price,image,desc,numbersold,total,refrefn,sale,opendiafn}) {
+    if(sale=="sale")
+    {
+        return (
+            <Card sx={{ maxWidth: 400 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image={image}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                   {desc}
+                  </Typography>
+                  <Typography gutterBottom variant="h6" component="div">
+                    {"You have : "+numbersold+' / '+total}
+                  </Typography>
+                  <Typography gutterBottom variant="h6" component="div">
+                    {price+'$'}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary" onClick={async () => {
+                    opendiafn(name,numbersold,price)
+        
+                }}
+        >
+                  {sale}
+                </Button>
+              </CardActions>
+            </Card>
+          );
 
-        }}
->
-          Buy
-        </Button>
-      </CardActions>
-    </Card>
-  );
+    }
+    else
+    {
+        return (
+            <Card sx={{ maxWidth: 400 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image={image}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                   {desc}
+                  </Typography>
+                  <Typography gutterBottom variant="h6" component="div">
+                    {"Sold : "+numbersold+' / '+total}
+                  </Typography>
+                  <Typography gutterBottom variant="h6" component="div">
+                    {price+'$'}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary" onClick={async () => {
+                    await buystock(userid,usermoney,price,name)
+                    refrefn()
+        
+                }}
+        >
+                  {sale}
+                </Button>
+              </CardActions>
+            </Card>
+          );
+
+    }
+  
 }
